c1d13549678df0e63b7e1e52ceb2e38a9d119f79
Germán Moreno <germanclarinet03@gmail.com>
2023-11-30 22:30:14 -0500

    Pruebas de red-social.service terminadas

--- src/red-social/red-social.entity.ts
+++ src/red-social/red-social.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+  import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { UsuarioEntity } from "../usuario/usuario.entity";
 
 @Entity()
--- src/red-social/red-social.service.spec.ts
+++ src/red-social/red-social.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedSocialService } from './red-social.service';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { RedSocialEntity } from './red-social.entity';
 
 describe('RedSocialService', () => {
   let service: RedSocialService;
@@ -17,5 +18,26 @@ describe('RedSocialService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('Deberia crear una red social', async ()=>{
+    const redsocial:RedSocialEntity = {
+      id:"1",
+      nombre:"facebook",
+      slogan:"Es gratis y lo sera para siempre",
+      usuarios:[]
+    }
+    const newRedsocial: RedSocialEntity = await service.createLibreria(redsocial);
+    expect(newRedsocial).not.toBeNull();
+  })
+
+  it('Deberia fallar al crear una red social', async ()=>{
+    const redsocial:RedSocialEntity = {
+      id:"1",
+      nombre:"facebook",
+      slogan:"",
+      usuarios:[]
+    }
+    await expect(()=> service.createLibreria(redsocial)).rejects.toHaveProperty("message","El slogan no puede estar vacio");
+  })
 
 });
